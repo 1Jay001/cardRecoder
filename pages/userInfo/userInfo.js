3b9d423d56190fbf48157b12ed5da8aa14f01860
copper <fsy1638@163.com>
2025-02-20 15:10:12 +0800

    用户信息提交修改
    openid并未传入后端

--- pages/userInfo/userInfo.js
+++ pages/userInfo/userInfo.js
@@ -1,64 +1,59 @@
-const app = getApp()
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
- 
- 
-Page({
-/**
-   * 页面的初始数据
-   */
-  data: {
-    avatarUrl: defaultAvatarUrl,
-    nickName: '',
- 
-  },
- 
-  onChooseAvatar(e) {
 
-    
-    // console.log("e::" +  e.detail.avatarUrl)
-    const avatarUrl = e.detail.avatarUrl;  // 获取头像 URL
-    this.setData({
-      avatarUrl: avatarUrl,  // 更新页面上的头像 URL
-    })
-    // console.log(this.data.avatarUrl)
-    
+Page({
+  data: {
+    avatarUrl: defaultAvatarUrl, // 默认头像
+    nickName: '',
+    userInfo: {} // 存储完整信息
   },
-  formSubmit(e) {
-    const nickName = e.detail.value.nickname;
-    
-    // 使用 setData 的回调函数，确保数据更新后再执行请求
+
+  // 获取头像
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
     this.setData({
+      avatarUrl: avatarUrl
+    })
+  },
+
+  // 实时获取昵称输入
+  onNicknameInput(e) {
+    this.setData({
+      nickName: e.detail.value
+    })
+  },
+
+  // 表单提交
+  formSubmit(e) {
+    const { avatarUrl, nickName } = this.data
+    const userInfo = {
+      avatarUrl: avatarUrl,
       nickName: nickName
+    }
+    // console.log("----------" + this.data.userInfo)
+
+    this.setData({
+      userInfo: userInfo
     }, () => {
-      console.log("更新后的数据：", this.data);
+      // console.log("更新后的数据：", this.data.userInfo)
 
       // 现在 this.data.nickName 已经更新，可以正确传递数据
       wx.request({
         url: 'http://localhost:8088/user',
         method: 'POST',
         data: {
-            openId: '12333',
-            nickName: this.data.nickName, // 确保这里能拿到最新的 nickName
-            atavarUrl: this.data.avatarUrl,
-            createTime: '',
-            lastLoginTime: ''
+          openId: '12333',
+          nickName: this.data.userInfo.nickName, 
+          avatarUrl: this.data.userInfo.avatarUrl,
+          createTime: '',
+          lastLoginTime: ''
         },
         success(res) {
-          console.log("后端返回数据：", res.data);
+          console.log("请求成功, 后端返回数据：", res.data);
         },
         fail(err) {
           console.error("请求失败：", err);
         }
-      });
-    });
-  },
-
- 
- 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    
-  }
+      })
+    }
+  )}
 })
